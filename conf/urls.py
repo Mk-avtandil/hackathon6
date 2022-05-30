1519241f32726afa36f0a90fc197a3ffd7f66577
avtandil <avtandilkgg@gmail.com>
2022-05-30 18:30:26 +0600

    fixed logout

--- conf/urls.py
+++ conf/urls.py
@@ -8,7 +8,7 @@ from app.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
-    path("api/v1/auth-base/", include("rest_framework.urls")),
-    path('api/v1/auth/', include('djoser.urls')),
-    path('api/v1/auth_token/', include('djoser.urls.authtoken')),
+    # path("api/v1/auth-base/", include("rest_framework.urls")),
+    # path('api/v1/auth/', include('djoser.urls')),
+    # path('api/v1/auth_token/', include('djoser.urls.authtoken')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
